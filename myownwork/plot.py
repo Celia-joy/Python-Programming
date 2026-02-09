232389c2c86509a9f6c93b07706b185f0952fcca
Celia <joyihirwecelia@gmail.com>
2026-02-09 16:28:38 +0200

    Updated python codes

---
+++ myownwork/plot.py
@@ -0,0 +1,12 @@
+#this code represents the charge
+#this given by Q= Qo sin * omega*t
+import numpy as np
+import matplotlib.pyplot as plt
+Qo = 5*(10**(-6))
+omega = 100 * np.pi
+T = 2*np.pi/omega
+t = np.linspace(0, 2*T, 100)
+Q = Qo*np.sin(omega*t)
+plt.plot(t, Q)
+plt.grid()
+plt.show()
